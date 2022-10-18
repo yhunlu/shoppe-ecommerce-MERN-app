f38cfa4e7edba14b5150d020c93091534b802c73
yhunlu <yhunlu87@gmail.com>
2022-10-18 22:27:07 +0300

    created ProductDetail component to integrate ProductScreen.

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,6 +1,8 @@
-export { default as Product } from './Product';
+export { default as LoadingBox } from './LoadingBox';
+export { default as MessageBox } from './MessageBox';
 export { default as Header } from './Header';
 export { default as Rating } from './Rating';
 export { default as NavBar } from './NavBar';
-export { default as LoadingBox } from './LoadingBox';
-export { default as MessageBox } from './MessageBox';
+export { default as Product } from './Product';
+export { default as ProductDetail } from './ProductDetail';
+
